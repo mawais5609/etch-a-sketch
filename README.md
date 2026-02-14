8907dcaa3e6eb835bdcc427744f23a76197bc8da
mawais5609 <mawais5609@gmail.com>
2026-02-15 01:04:03 +0500

    Project Completed.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # etch-a-sketch
+
+This is a project through my journey with The odin Project.
+With the help of this Project i strengthened my Skills HTML CSS and Basic Javascript with DOM Manipulation.
--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Hello, World!")
+const resizeButton = document.querySelector(".heading > button")
+
+resizeButton.addEventListener("click", () => {
+    const size = prompt("Enter Grid Size <= 40", "")
+
+    if (size > 40) {
+        alert("Size must be less than or equal to 40")
+    } else {
+        createGrid(size, size)
+    }
+})
+
+function createGrid(rows, cols) {
+    const gridContainer = document.querySelector(".grid-container")
+    gridContainer.innerHTML = ""
+
+    for (let r = 0; r < rows; r++) {
+        const row = document.createElement("div")
+        row.classList.add("row")
+
+        for (let c = 0; c < cols; c++) {
+            const cell = document.createElement("div")
+            cell.classList.add("cell")
+
+            cell.addEventListener("mouseenter", () => {
+                let r = Math.floor(Math.random() * 256)
+                let g = Math.floor(Math.random() * 256)
+                let b = Math.floor(Math.random() * 256)
+
+                cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+            })
+
+            row.appendChild(cell)
+        }
+
+        gridContainer.appendChild(row)
+    }
+}
+
+createGrid(16, 16)
